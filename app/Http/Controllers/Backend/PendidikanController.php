d4f7f30e6deac7bdff22b3ef9ba0c901f02562cb
fachry-15 <93858334+fachry-15@users.noreply.github.com>
2023-04-13 14:13:21 +0700

    first commit

---
+++ app/Http/Controllers/Backend/PendidikanController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Http\Controllers\Controller;
+use App\Pendidikan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class PendidikanController extends Controller
+{
+    public function index()
+    {
+        $pendidikan = Pendidikan::get();
+        return view('backend.pendidikan.index', compact('pendidikan'));
+    }
+    public function create()
+    {
+        $pendidikan = null;
+        return view('backend.pendidikan.create', compact('pendidikan'));
+    }
+    public function store(Request $request)
+    {
+        Pendidikan::create($request->all());
+        return redirect()->route('pendidikan.index')
+            ->with('success', 'Data Pendidikan Baru Telah Berhasil Disimpan');
+    }
+    public function edit(Pendidikan $pendidikan)
+    {
+        return view('backend.pendidikan.create', compact('pendidikan'));
+    }
+    public function update(Request $request, Pendidikan $pendidikan)
+    {
+        $pendidikan->update($request->all());
+
+        return redirect()->route('pendidikan.index')
+            ->with('success', 'Pendidikan berhasil diperbaharui');
+    }
+    public function destroy(Pendidikan $pendidikan)
+    {
+        $pendidikan->delete();
+        return redirect()->route('pendidikan.index')
+            ->with('success', 'Data Pendidikan berhasil dihapus');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['namespace' => 'Backend'], function () {
+    Route::resource('dashboard', 'DashboardController');
+});
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['namespace' => 'Backend'], function () {
+    Route::resource('dashboard', 'DashboardController');
+    Route::resource('pendidikan', 'PendidikanController');
+    Route::resource('pengalaman_kerja', 'PengalamanKerjaController');
+});
+
+Route::group(['namespace' => 'Backend'], function () {
+    Route::resource('dashboard', 'DashboardController');
+    Route::resource('pendidikan', 'PendidikanController');
+});
